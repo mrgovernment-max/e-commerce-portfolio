e1d216a715300b8b1f5bb5fed08b4a5dd3172d7d
Emman <efenteng1@gmail.com>
2024-12-01 01:10:16 +0000

    added product details page

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-function addToCart(productName, productPrice, imagesrc, quantity, size) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push({
-    name: productName,
-    price: productPrice,
-    image: imagesrc,
-    quantity: quantity,
-    size: size,
-  });
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert(
-    `${quantity} [${size}] ${productName}(s) have been added to your cart!`
-  );
-  displayTotalitemsincart();
-}
 function filterProducts() {
   // Temporarily disable AOS
   AOS.refresh({ disable: true });
@@ -38,7 +23,37 @@ function filterProducts() {
 function displayTotalitemsincart() {
   let cart = JSON.parse(localStorage.getItem("cart")) || [];
   const cartTotal = cart.length;
-  const domDisplaycartTota = document.getElementById("total-cart-items");
-  domDisplaycartTota.innerHTML = cartTotal;
+  const domDisplaycartTotal = document.getElementById("total-cart-items");
+  domDisplaycartTotal.innerHTML = cartTotal;
 }
 window.onload = displayTotalitemsincart;
+
+function productDetails(
+  img1,
+  img2,
+  img3,
+  img4,
+  rating,
+  info,
+  availability,
+  brandimg,
+  brandinfo,
+  productName,
+  productPrice
+) {
+  // Storing product details in localStorage
+  localStorage.setItem("img1", img1);
+  localStorage.setItem("img2", img2);
+  localStorage.setItem("img3", img3);
+  localStorage.setItem("img4", img4);
+  localStorage.setItem("rating", rating);
+  localStorage.setItem("info", info);
+  localStorage.setItem("availability", availability);
+  localStorage.setItem("brandimg", brandimg);
+  localStorage.setItem("brandinfo", brandinfo);
+  localStorage.setItem("name", productName);
+  localStorage.setItem("price", productPrice);
+
+  // Navigating to the product details page
+  window.location.href = "productdetails.html";
+}
